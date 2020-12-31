8aa2bef1d36062601192c6873bd1583c28454be5
Luís Pereira <luis.artur.pereira@gmail.com>
2020-12-31 13:09:51 +0000

    Adds an CMake FindXTerm module

---
+++ cmake/find-modules/FindXTerm.cmake
@@ -0,0 +1,52 @@
+#=============================================================================
+# Copyright 2020 Luís Pereira <luis.artur.pereira@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+# FindXTerm
+#
+# Try to find the XTerm executable
+#
+# This will define the following variables:
+#
+# `XTerm_FOUND`
+#     True if XTerm executable is available.
+#
+# `XTerm_EXECUTABLE`
+#     The XTerm executable.
+#
+
+# Find XTerm executable
+find_program(XTerm_EXECUTABLE NAMES xterm)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(XTerm
+    FOUND_VAR
+        XTerm_FOUND
+    REQUIRED_VARS
+        XTerm_EXECUTABLE
+)
+
+mark_as_advanced(XTerm_EXECUTABLE)
